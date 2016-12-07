191d45db6fdad79c81a99053f93c49f9ce8c147d
rsgc-murphy-o <omurphy@rsgc.on.ca>
2016-12-07 14:42:42 -0500

    Got Mario code working. and added the next playground.

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -11,3 +11,28 @@
  */
 
 // Add your code below
+
+func levelCost(heights: [Int], maxJump: Int) -> Int {
+    var energyUse = 0
+    
+    heights
+    for i in 0...heights.count-2{
+        
+        let currentPlatformHeight = heights[i]
+        let nextPlatformHeight = heights[i + 1]
+        let heightDifference = abs(nextPlatformHeight - currentPlatformHeight)
+        // trying to figure out the use of energy.
+        if heightDifference > 0 {
+            
+            if heightDifference > maxJump {
+                return -1
+            }
+            energyUse += 2 * heightDifference
+        } else {
+            energyUse += 1
+        }
+    }
+    return energyUse
+}
+// expected enery use is 4
+levelCost(heights: [1,1,6,1,1], maxJump: 2)
